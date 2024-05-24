6d083c8bd49bfee7ad1bb2b969a671acb09b3464
sangbijaksana <aldihilmanr@yahoo.com>
2024-05-24 15:42:58 +0700

    Add check for existing email

--- api_auth.go
+++ api_auth.go
@@ -1,26 +1,52 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Register(w http.ResponseWriter, r *http.Request) {
 	var user User
 	json.NewDecoder(r.Body).Decode(&user)
+	w.Header().Set("Content-Type", "application/json")
 
 	if user.Password != user.PasswordConf {
-		http.Error(w, "Password confirmation doesn't match", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Password doesn't match"})
 		return
 	}
 
-	_, err := db.Exec("INSERT INTO users (username, password) VALUES (?, ?)", user.Username, user.Password)
+	hashPass, err := hashPassword(user.Password)
 	if err != nil {
-		http.Error(w, "Could not register user", http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Failed to hash password"})
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
+	// Check if username already exists
+	var existingUser User
+	err = db.QueryRow("SELECT id FROM users WHERE username = ?", user.Username).Scan(&existingUser.ID)
+	if err == nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Email already taken"})
+		return
+	} else if err != sql.ErrNoRows {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Could not check email"})
+		return
+	}
+
+	_, err = db.Exec("INSERT INTO users (username, password) VALUES (?, ?)",
+		user.Username, hashPass)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Could not register user"})
+		return
+	}
+
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(map[string]string{"message": "Register user success"})
 }
@@ -28,27 +54,48 @@ func Register(w http.ResponseWriter, r *http.Request) {
 func Login(w http.ResponseWriter, r *http.Request) {
 	var user User
 	json.NewDecoder(r.Body).Decode(&user)
+	w.Header().Set("Content-Type", "application/json")
 
 	var dbUser User
 	err := db.QueryRow("SELECT id, username, password FROM users WHERE username = ?", user.Username).Scan(&dbUser.ID, &dbUser.Username, &dbUser.Password)
-	if err != nil || dbUser.Password != user.Password {
-		http.Error(w, "Invalid username or password", http.StatusUnauthorized)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Internal server error"})
+		return
+	}
+
+	if dbUser.ID <= 0 || !checkPasswordHash(user.Password, dbUser.Password) {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Invalid username or password"})
 		return
 	}
 
 	token, err := GenerateToken(user.Username)
 	if err != nil {
-		http.Error(w, "Could not generate token", http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Could not generate token"})
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"jwt_token": token})
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "Login success", "jwt_token": token})
 }
 
 func Logout(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]string{"message": "Logout success"})
+}
+
+func hashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func checkPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
 )
